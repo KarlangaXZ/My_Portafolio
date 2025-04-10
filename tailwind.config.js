ff575e517ed709c03c0478d173008403fe5e38fc
KarlangaXZ <carlos.josewin@gmail.com>
2025-04-10 12:37:35 -0400

    Some change on project section

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,12 +3,6 @@ module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
     extend: {
-      colors: {
-        primary: '#0070f3',
-        secondary: '#6c757d',
-        dark: '#212529',
-        light: '#f8f9fa',
-      },
       fontFamily: {
         sans: ['Poppins', 'sans-serif'],
       },
